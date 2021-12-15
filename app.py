0f39bf62f5de7b58462074c0d3a966b38205c583
Austin Jackson <themacphage@gmail.com>
2021-12-14 19:13:36 -0600

    updated

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ from sklearn.cluster import DBSCAN
 app = Flask(__name__)
 cors = CORS(app)
 app.config['UPLOAD_FOLDER'] = '/tmp'
-ALLOWED_EXTENSIONS = set([''])
+BASEURL = os.environ['BASEURL'] if 'BASEURL' in os.environ else '/fuckthisproject'
 
-@app.route('/fuckthisproject/')
+@app.route(f'{BASEURL}/')
 @cross_origin()
 def hello():
-  return '''
+  return f'''
   <form>
   <div>
     <label>Select file to upload</label>
@@ -25,32 +25,42 @@ def hello():
   </form>
   <img src="" />
   <script>
-  async function handleSubmit(event) {
+  async function handleSubmit(event) {{
     console.log(event);
     event.preventDefault();
     const file = event.target[0].files[0];
     const formData = new FormData();
     formData.append('file', file);
+    formData.append('eps', 0.4);
+    formData.append('min_samples', 120);
     console.log(formData);
-    await fetch('/dbscan', {
+    await fetch('{BASEURL}/dbscan', {{
       method: 'POST',
       body: formData
-    });
-    document.querySelector('img').setAttribute('src','/uploads/'+file.name);
-  }
+    }});
+    document.querySelector('img').setAttribute('src','{BASEURL}/uploads/'+file.name);
+  }}
   document.querySelector('form').addEventListener('submit', handleSubmit);
   </script>
   '''
 
-@app.route('/fuckthisproject/uploads/<filename>')
+@app.route(f'{BASEURL}/uploads/<filename>')
 @cross_origin()
 def uploaded_file(filename):
   return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-@app.route('/fuckthisproject/dbscan', methods=['POST'])
+@app.route(f'{BASEURL}/dbscan', methods=['POST'])
 @cross_origin()
 def upload_file():
   print(request.files)
+  GIVEN_EPS = 0.3
+  GIVEN_MIN_SAMPLES = 100
+  if 'eps' in request.form:
+    GIVEN_EPS = float(request.form['eps'])
+    print(f'GIVEN_EPS = {GIVEN_EPS}')
+  if 'min_samples' in request.form:
+    GIVEN_MIN_SAMPLES = int(request.form['min_samples'])
+    print(f'GIVEN_MIN_SAMPLES = {GIVEN_MIN_SAMPLES}')
   if 'file' not in request.files:
     return 'there is no file in form!'
   file = request.files['file']
@@ -59,7 +69,7 @@ def upload_file():
 
   img = cv2.imread(path)
   Z = np.float32(img.reshape((-1,3)))
-  db = DBSCAN(eps=0.3, min_samples=100).fit(Z[:,:2])
+  db = DBSCAN(eps=GIVEN_EPS, min_samples=GIVEN_MIN_SAMPLES).fit(Z[:,:2])
   res2 = np.uint8(db.labels_.reshape(img.shape[:2]))
 
   cv2.imwrite(path, res2)
