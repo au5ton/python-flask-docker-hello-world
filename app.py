aa7cc0a120245f26d7c6189d589d91823d87ced9
Austin Jackson <themacphage@gmail.com>
2021-12-14 18:05:05 -0600

    get this bread

--- app.py
+++ app.py
@@ -1,13 +1,70 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory
+from flask_cors import CORS, cross_origin
 import os
 
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import DBSCAN
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config['UPLOAD_FOLDER'] = '/tmp'
+ALLOWED_EXTENSIONS = set([''])
 
-@app.route("/")
+@app.route('/')
+@cross_origin()
 def hello():
-    return "Flask inside Docker!!"
+  return '''
+  <form>
+  <div>
+    <label>Select file to upload</label>
+    <input type="file">
+  </div>
+  <button type="submit">Convert</button>
+  </form>
+  <img src="" />
+  <script>
+  async function handleSubmit(event) {
+    console.log(event);
+    event.preventDefault();
+    const file = event.target[0].files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+    console.log(formData);
+    await fetch('/dbscan', {
+      method: 'POST',
+      body: formData
+    });
+    document.querySelector('img').setAttribute('src','/uploads/'+file.name);
+  }
+  document.querySelector('form').addEventListener('submit', handleSubmit);
+  </script>
+  '''
 
+@app.route('/uploads/<filename>')
+@cross_origin()
+def uploaded_file(filename):
+  return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/dbscan', methods=['POST'])
+@cross_origin()
+def upload_file():
+  print(request.files)
+  if 'file' not in request.files:
+    return 'there is no file in form!'
+  file = request.files['file']
+  path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+  file.save(path)
+
+  img = cv2.imread(path)
+  Z = np.float32(img.reshape((-1,3)))
+  db = DBSCAN(eps=0.3, min_samples=100).fit(Z[:,:2])
+  res2 = np.uint8(db.labels_.reshape(img.shape[:2]))
+
+  cv2.imwrite(path, res2)
+  return path
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(debug=True,host='0.0.0.0',port=port)
